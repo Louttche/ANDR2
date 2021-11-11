f84767811ecbb28cc2ddfb6ac43eb446a87933fa
Louttche <lnicolaidou.katerina@gmail.com>
2021-11-11 13:15:06 +0100

    Added Map dependencies (+ MapFragment)

--- app/src/main/java/com/example/andr2_app/MainActivity.java
+++ app/src/main/java/com/example/andr2_app/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.maps.MapFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MapFragment mf = new MapFragment();
+        getFragmentManager().beginTransaction().add(R.id.frameLayout_map, mf).commit();
     }
 }
