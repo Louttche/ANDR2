5a73f044b87fa65a600ce12e8442664e4cdf338c
Justice Dreischor <t-justice98@hotmail.com>
2021-11-23 21:05:26 +0100

    disabled night mode

--- app/src/main/java/com/example/stalkr/activities/AuthActivity.kt
+++ app/src/main/java/com/example/stalkr/activities/AuthActivity.kt
@@ -2,6 +2,7 @@ package com.example.stalkr.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.stalkr.R
 import com.example.stalkr.fragments.LoginFragment
 
@@ -9,7 +10,7 @@ class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
-
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         val loginFragment = LoginFragment()
         val fragmentTransaction = supportFragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.fragmentContainerViewAuth, loginFragment, "LoginFragment")
