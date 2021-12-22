26ee90c4bbbaf7f29488cccabf4a6d94dfb5972e
Dimitrov, Dimitar D.S <i402384@fhict.nl>
2021-12-22 16:45:35 +0200

    Image loads on first open of info window of marker

--- app/src/main/java/com/example/stalkr/CustomInfoWindowForGoogleMap.kt
+++ app/src/main/java/com/example/stalkr/CustomInfoWindowForGoogleMap.kt
@@ -2,6 +2,7 @@ package com.example.stalkr
 
 import android.app.Activity
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.util.Log
 
 import android.view.View
@@ -9,12 +10,15 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
 import com.example.stalkr.data.InfoWindowData
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.Marker
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
+import javax.sql.DataSource
 
 class CustomInfoWindowForGoogleMap(context: Context) : GoogleMap.InfoWindowAdapter {
     private var mContext = context
@@ -27,10 +31,13 @@ class CustomInfoWindowForGoogleMap(context: Context) : GoogleMap.InfoWindowAdapt
         tv_username.text = marker.title
 
         val imageUrl = marker.snippet
-        val iv_photo : ImageView = mInfoView.findViewById(R.id.iv_user_photo)
-        Glide.with(this.mContext)
-            .load(imageUrl)
-            .into(iv_photo)
+        if (imageUrl !== "") {
+            val iv_photo : ImageView = mInfoView.findViewById(R.id.iv_user_photo)
+            Glide.with(this.mContext)
+                .load(imageUrl)
+                .listener(MarkerCallback(marker))
+                .into(iv_photo)
+        }
 
         val btn_viewprofile : Button = mInfoView.findViewById(R.id.btn_view_profile)
         btn_viewprofile.setOnClickListener{
@@ -49,5 +56,43 @@ class CustomInfoWindowForGoogleMap(context: Context) : GoogleMap.InfoWindowAdapt
 
     override fun getInfoWindow(marker: Marker): View? {
         return null
+    }
+}
+
+class MarkerCallback internal constructor(marker: Marker?) :
+    RequestListener<Drawable> {
+
+    var marker: Marker? = null
+
+    private fun onSuccess() {
+        if (marker != null && marker!!.isInfoWindowShown) {
+            marker!!.hideInfoWindow()
+            marker!!.showInfoWindow()
+        }
+    }
+
+    init {
+        this.marker = marker
+    }
+
+    override fun onLoadFailed(
+        e: GlideException?,
+        model: Any?,
+        target: com.bumptech.glide.request.target.Target<Drawable>?,
+        isFirstResource: Boolean
+    ): Boolean {
+        Log.e(javaClass.simpleName, "Error loading thumbnail! -> $e")
+        return false
+    }
+
+    override fun onResourceReady(
+        resource: Drawable?,
+        model: Any?,
+        target: com.bumptech.glide.request.target.Target<Drawable>?,
+        dataSource: com.bumptech.glide.load.DataSource?,
+        isFirstResource: Boolean
+    ): Boolean {
+        onSuccess()
+        return false
     }
 }
